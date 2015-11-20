11ae81cfdc547877ed672d60ff8dcdf582020384
jiangyangdong <jiangyangdong@yunzhisheng.cn>
2015-11-20 14:33:56 +0800

    should work

--- src/main.go
+++ src/main.go
@@ -1,38 +1,70 @@
 package main
 
-import(
-	"log"
-	_ "github.com/weilaihui/fdfs_client"
-	"flag"
+import (
 	"encoding/json"
+	"flag"
+	"github.com/weilaihui/fdfs_client"
+	"log"
+	"sync"
+	"sync/atomic"
+	"math/rand"
+	"time"
+	"net/http"
+	//"github.com/jtolds/gls"
+	"os"
+	"os/signal"
+	"runtime"
+	"fmt"
 )
 
-type testDesc struct{
+type testDesc struct {
 	TotalCount int
-	Write struct{
+	Parallel int
+	Write struct {
 		Pct int
-		Cfg []struct{
-			Avg int
-			Max int
-			Min int
-			Pct int
-		}
+		Count int
+		Cfg []writeCfg
 	}
-	Read struct{
+	Read struct {
 		Pct int
-    }
+		Count int
+	}
 }
 
-func main(){
+type task struct{
+	//readId string
+	writeLen int
+}
+type writeCfg struct{
+	Max int
+	Min int
+	Pct int
+	Count int
+}
+
+type reportStr struct{
+	Start time.Time
+	End time.Time
+	Duration time.Duration
+	AvgWriteDuration time.Duration
+	AvgReadDuration time.Duration
+	TotalWriteBytes int64
+	TotalReadBytes int64
+	TotalWriteTime int64
+	TotalReadTime int64
+}
+
+func main() {
 	defaultTestDesc := `
 {
-	"TotalCount":100000,
+	"TotalCount":1000,
+	"Parallel":10,
 	"Write":{
-		"Pct":99,
+		"Pct":90,
 		"Cfg":[
-			{"Avg":7, "Max":15, "Min":3, "Pct":95},
-			{"Avg":200, "Max":300, "Min":100, "Pct":3},
-			{"Avg":500, "Max":800, "Min":300, "Pct":2}
+			{"Max":15000, "Min":3000, "Pct":95},
+			{"Max":300000, "Min":100000, "Pct":3},
+			{"Max":800000, "Min":300000, "Pct":2}
 		]
 	},
 	"Read":{
@@ -40,31 +72,216 @@ func main(){
 	}
 }
 `
-	fdfsAddress := flag.String("fa", "", "(must)fastDFS connection string(like 10.0.0.5:22313)")
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func(){
+		for _ = range c {
+			RecogStack()
+		}
+	}()
+
+	var report reportStr
+	fdfsCfgFile := flag.String("fa", "", "(must)fastDFS config file path")
 	origUsage := flag.Usage
-	flag.Usage = func(){
+	flag.Usage = func() {
 		log.Println("the default test description is")
 		log.Println(defaultTestDesc)
 		origUsage()
 	}
 	testDescStr := flag.String("td", "see default test description", "(opt)test description")
-
 	flag.Parse()
-	if(*fdfsAddress==""){
+	if *fdfsCfgFile == "" {
 		flag.Usage()
 		return
 	}
-	log.Println("to use fastDFS address", *fdfsAddress)
-	if(*testDescStr=="see default test description"){
+	log.Println("to use fastDFS config file", *fdfsCfgFile)
+	if *testDescStr == "see default test description" {
 		log.Println("to use default test description")
 		*testDescStr = defaultTestDesc
 	}
 
 	td := testDesc{}
-
-	if err:=json.Unmarshal([]byte(*testDescStr), &td);err != nil{
+	if err := json.Unmarshal([]byte(*testDescStr), &td); err != nil {
 		log.Println(err)
 		flag.Usage()
 		return
 	}
+	td.Write.Count = td.TotalCount*td.Write.Pct / 100
+	td.Read.Count = td.TotalCount*td.Read.Pct / 100
+	maxBufLen := 0
+	for _,c:= range td.Write.Cfg{
+		if maxBufLen < c.Max{
+			maxBufLen = c.Max
+		}
+		c.Count = c.Pct * td.Write.Count
+	}
+
+	var err error
+
+	writeBuf := make([]byte, maxBufLen)
+	taskChan := make(chan task, td.Parallel * 2)
+	workerWg := sync.WaitGroup{}
+
+	writeCount := int64(0)
+	readCount := int64(0)
+
+	var client *fdfs_client.FdfsClient
+	if client,err=fdfs_client.NewFdfsClient("client.conf");err != nil{
+		log.Fatal(err)
+	}
+
+	for i:=0;i<td.Parallel;i++{
+		workerWg.Add(1)
+		 go func(i int){
+			 //mgr:=gls.NewContextManager();
+			 //mgr.SetValues(gls.Values{"id":i}, func() {
+				 defer func() {
+					 log.Println("to quit worker ", i)
+					 workerWg.Done()
+				 }()
+
+				 type writeRslt struct{
+					 id string
+					 size int
+				 }
+				 wroteIDS := make([]writeRslt, 0)
+				 idsLen := 0
+				 rnd := rand.New(rand.NewSource(int64(time.Now().Nanosecond())))
+
+				 loop:
+				 for {
+					 //log.Println(i, " waitting task")
+					 select {
+					 //for t:= range taskChan{
+					 case t, ok := <-taskChan:
+						 if t.writeLen > 0 {
+							 wc := int(atomic.AddInt64(&writeCount, 1))
+							 rd := int(atomic.LoadInt64(&readCount))
+							 start := time.Now()
+							 //log.Println(i, "to write ", t.writeLen)
+							 id := doWrite(client, writeBuf[0:t.writeLen])
+							 //log.Println(i, "done write ", t.writeLen)
+							 atomic.AddInt64(&report.TotalWriteTime, int64(time.Now().Sub(start)))
+							 wroteIDS = append(wroteIDS, writeRslt{id,int(t.writeLen)})
+							 idsLen++
+							 if reachPct(int64(td.Write.Pct), int64(wc), int64(rd + wc)) {
+								 atomic.AddInt64(&readCount, 1)
+								 start := time.Now()
+								 id := wroteIDS[rnd.Int31n(int32(idsLen))]
+								 //log.Println(i, "to read ", id.id)
+								 inc := doRead(client, id.id)
+								 //log.Println(i, "done read ", id.id)
+								 if inc != id.size {
+									 log.Fatalln("wrote ", id.size, " but read ", inc, " for file:", id.id)
+								 }
+								 atomic.AddInt64(&report.TotalReadTime, int64(time.Now().Sub(start)))
+								 atomic.AddInt64(&report.TotalReadBytes, int64(inc))
+							 }
+						 }
+						 if !ok {
+							 break loop
+						 }
+					 }
+				 //}
+			 }
+		}(i)
+	}
+
+	writeFall := make([]*writeCfg, 100)
+	for idx,i:=0,0;i<len(td.Write.Cfg);i++{
+		for j:=0;j<td.Write.Cfg[i].Pct;j++ {
+			writeFall[idx]=&td.Write.Cfg[i]
+			idx++
+		}
+	}
+	if writeFall[len(writeFall) - 1] == nil{
+		log.Fatalln("write cfg pcts not 100%")
+	}
+
+	rnd := rand.New(rand.NewSource(int64(time.Now().Nanosecond())))
+	report.Start = time.Now()
+	for i:=0;i<td.Write.Count;i++{
+		c := writeFall[int(rnd.Int31n(100))]
+		t := task{}
+		t.writeLen = c.Min + int(rnd.Int31n(int32(c.Max - c.Min)))
+		//log.Println("submit write task len:", t.writeLen)
+		taskChan <- t
+		report.TotalWriteBytes += int64(t.writeLen)
+	}
+	close(taskChan)
+
+
+	log.Println("to wait task finish")
+	workerWg.Wait()
+	report.End = time.Now()
+	report.Duration = report.End.Sub(report.Start)
+	report.AvgReadDuration = time.Duration(report.TotalReadTime / readCount)
+	report.AvgWriteDuration = time.Duration(report.TotalWriteTime / writeCount)
+	log.Println("total write:",writeCount, " total read:",readCount)
+	if rsp,err:=json.MarshalIndent(report,"","\t");err==nil{
+		log.Println(string(rsp))
+		log.Println("visit :4901 for report")
+
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write(rsp)
+		})
+
+		//log.Fatal(http.ListenAndServe(":4901", nil))
+	}else{
+		log.Fatal("marshal err:", err)
+	}
+}
+
+func reachPct(pct,cnt,total int64) bool{
+	return cnt > (total * pct / 100)
+}
+
+func doRead(client *fdfs_client.FdfsClient, id string) int{
+	if rsp,err:=client.DownloadToBuffer(id, 0, 0);err != nil || rsp.DownloadSize < 0{
+		log.Fatalln("download ", id, " error:", err)
+		return -1
+	}else{
+		return int(rsp.DownloadSize)
+	}
+}
+
+func doWrite(client *fdfs_client.FdfsClient, data []byte) string{
+	if rsp, err :=client.UploadByBuffer(data, ".raw");err != nil{
+		log.Fatalln("upload error:", err)
+		return ""
+	}else {
+		return rsp.RemoteFileId
+	}
+}
+
+func isexist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
+
+func outputStack(fname string) {
+	buf := make([]byte, 1<<20)
+	runtime.Stack(buf, true)
+
+	fd, err := os.Create(fname)
+	if err != nil {
+		fmt.Printf("%s\n", err.Error())
+	}
+	defer fd.Close()
+
+	fd.Write(buf)           //write file
+	fmt.Printf(string(buf)) //print stdout
+}
+
+func RecogStack() {
+	count := 0
+	cur_name := ""
+	for ; ; count++ {
+		cur_name = fmt.Sprintf("debug_stack.log.%d", count)
+		if !isexist(cur_name) {
+			break
+		}
+	}
+	outputStack(cur_name)
 }
